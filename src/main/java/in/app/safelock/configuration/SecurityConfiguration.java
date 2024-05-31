98afcb703f06e60f399d4c4e089b4137efba9427
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-06-01 04:38:20 +0530

    commit
    google log in done but not saved in db

--- src/main/java/in/app/safelock/configuration/SecurityConfiguration.java
+++ src/main/java/in/app/safelock/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -52,6 +53,13 @@ public class SecurityConfiguration {
         });
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+
+        httpSecurity.oauth2Login(Customizer.withDefaults());
+        // httpSecurity.oauth2Login(oauth -> {
+        //     oauth.loginPage("/login");
+        //     oauth.successHandler(handler);
+        // });
 
         httpSecurity.logout(logoutForm -> {
             logoutForm.logoutUrl("/logout");
